6a2ab4694b6c37229aba9fca43d8dfdc222504c0
leonleerl <leon.runlongli@gmail.com>
2024-07-30 22:31:42 +0800

    write tailwind code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import AppleHome from "./AppleHome";
 
 function App() {
+  const [list, setList] = useState([
+    { id: 1001, name: "Vue" },
+    { id: 1002, name: "React" },
+    { id: 1003, name: "Angular" },
+  ]);
+  const [id, setId] = useState(1004);
+  const msg = "Ye!!!s";
+  const isLogin = true;
+  const articleType = "spo2rts";
+  function getAge() {
+    return 12;
+  }
+  function getArticle(type) {
+    if (type === "politics") {
+      return "POLITICS";
+    } else if (type === "sports") {
+      return "SPORTS";
+    } else {
+      return "None";
+    }
+  }
+  const clickHandler = () => {
+    setList([...list, { id: id, name: "Avalonia" + id }]);
+    setId(id + 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AppleHome />
+      <div>Hello!!</div>
+      <div>{msg}</div>
+      <div>{getAge()}</div>
+      <div>
+        <ul>
+          {list.map((item) => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+        </ul>
+      </div>
+      <div>{isLogin ? <div>login</div> : <div>not login</div>}</div>
+      <div>This is a {getArticle(articleType)} passage.</div>
+      <button onClick={() => clickHandler()}>Click me</button>
+      <div>
+        <button>asc</button>
+        <button>des</button>
+      </div>
+    </>
   );
 }
 
---
+++ src/AppleHome.js
@@ -0,0 +1,52 @@
+function AppleHome() {
+  return (
+    <>
+      <div class="container mx-auto">
+        <img
+          src="https://www.apple.com/v/macbook-pro/ak/images/overview/themes/performance/mbp_16_screen_battery_endframe__ez8pox0tqtkm_large.jpg"
+          class="shadow-2xl rounded-xl"
+        />
+        <div class="my-8 overflow-hidden rounded-lg border shadow-lg sm:flex sm:flex-wrap">
+          <div class="group border-b p-6 hover:bg-gray-800 sm:w-1/2 sm:border-r md:w-1/4">
+            <h2 class="title">Xcode</h2>
+            <p class="text-gray-500 group-hover:text-white">
+              Now apps just soar — from your go-to productivity apps to your
+              favourite games and hardest working pro apps.
+            </p>
+          </div>
+          <div class="group border-b p-6 hover:bg-gray-800 sm:w-1/2 md:w-1/4 md:border-r">
+            <h2 class="mb-2 text-xl font-bold group-hover:text-white">
+              Unity Editor
+            </h2>
+            <p class="text-gray-500 group-hover:text-white">
+              With thousands of apps working pro apps. With thousands of apps
+              working pro apps. With thousands of apps working pro apps.
+            </p>
+          </div>
+          <div class="group border-b p-6 hover:bg-gray-800 sm:w-1/2 sm:border-b-0 sm:border-r md:w-1/4">
+            <h2 class="mb-2 text-xl font-bold group-hover:text-white">
+              Compiler
+            </h2>
+            <p class="text-gray-500 group-hover:text-white">
+              apps optimiver before. Now apps just soar — from your go-to
+              productivity apps to your favourite games and hardest working pro
+              apps.
+            </p>
+          </div>
+          <div class="group border-b p-6 hover:bg-gray-800 sm:w-1/2 md:w-1/4">
+            <h2 class="mb-2 text-xl font-bold group-hover:text-white">
+              Docker
+            </h2>
+            <p class="text-gray-500 group-hover:text-white">
+              M3 chips accelerate performance like never before. Now apps just
+              soar — from your go-to productivity apps to your favourite games
+              and hardest working pro apps.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default AppleHome;
